fcda3ddb8c0e51cc14b9a12ca47bcc3aed8c170d
Bryantle <bryantl@berkeley.edu>
2018-01-14 14:41:12 -0800

    Created an @IBAction method with the record button as its sender and added a hardcoded debug.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,16 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func recordButton(_ sender: UIButton) {
+        print("Hello World")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
